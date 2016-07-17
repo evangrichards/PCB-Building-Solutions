3506cf311acba3af2362ff40729951f923fca226
Evan Richards <Ev@Evans-MBP.home>
2016-07-17 11:24:31 +0100

    contact page started

---
+++ wp-content/themes/PCB/contact.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php include('parts/hero.php'); ?>
+
+<main id="content" class="c-1 left">
+
+  <div class="page-content">
+    <section class="grey-content c-1">
+      <div class="inner">
+        <div class="position-me">
+          <div id="fixed">
+
+            <div class="inside-left">
+              <p><strong>Mobile:</strong></p>
+              <?php if (get_field('secondary_telephone','option')) {?>
+              <p><strong>Landline:</strong></p>
+              <?php } ?>
+              <p><strong>Email:</strong></p>
+            </div>
+
+            <div class="inside-right">
+              <p><a href="tel:<?php the_field('primary_telephone', 'option'); ?>" class="tel" title="Call today for your free quote"><?php the_field('primary_telephone', 'option'); ?></a></p>
+              <?php if (get_field('secondary_telephone','option')) {?>
+                <p><a href="tel:<?php the_field('secondary_telephone', 'option'); ?>" class="tel" title="Call today for your free quote"><?php the_field('secondary_telephone', 'option'); ?></a></p>
+              <?php } ?>
+              <p><a href="mailto:<?php the_field('email_address', 'option'); ?>" title="Email us today for your free quote"><?php the_field('email_address', 'option'); ?></a></p>
+            </div>
+
+            <?php if (get_field('disclaimer')) {?>
+              <div class="c-1 disclaimer">
+                <?php the_field('disclaimer'); ?>
+              </div>
+            <?php } ?>
+
+          </div>
+          <div id="fluid">
+            Form
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <?php include('parts/testimonials.php'); ?>
+
+</main>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
